76f396c58e93b02f537b09a34cae9b48207d6294
svitlana1dev <ponomarchuk.svitlana.dev@gmail.com>
2025-04-07 22:02:56 +0300

    added socket and frontend

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,34 @@
 import express from "express";
+import http from "http";
+import cors from "cors";
+import { Server, Socket } from "socket.io";
 
 const app = express();
 const port = 3000;
+const server = http.createServer(app);
 
 app.use(express.json());
+app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
-app.listen(port, () => {
+io.on("connection", (socket: Socket) => {
+  console.log("a user connected", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected", socket.id);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+server.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
